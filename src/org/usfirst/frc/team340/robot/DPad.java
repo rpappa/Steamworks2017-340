c5ff57b3da8346628fe57508f2decfeca37a3e88
wundrweapon <whhacker.dcx@gmail.com>
2017-01-16 11:21:34 -0500

    Stole 2016 DPad code

---
+++ src/org/usfirst/frc/team340/robot/DPad.java
@@ -0,0 +1,66 @@
+package org.usfirst.frc.team340.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+
+public class DPad extends Button {
+    private Joystick joystick;
+    private int direction;
+    
+    /**
+     * Class for dealing with the D-Pad
+     * aka PoV hat using enum
+     * @param joy {@link Joystick} in
+     * question
+     * @param dir {@link Direction} enum
+     * to use (because memorizing numbers
+     * is hard)
+     */
+    public DPad(Joystick joy, Direction dir) {
+    	this(joy, dir.getDirection());
+    }
+    	
+    /**
+     * Class for dealing with the D-Pad
+     * aka PoV hat
+     * @param joy the joystick
+     * @param direction angle the hat
+     * must be equal to
+     */
+    public DPad(Joystick joy, int direction) {
+    	joystick = joy;
+    	this.direction = direction;
+    }
+    
+    /**
+     * Gets whether or not the hat's angle
+     * is equal to the given angle
+     * @return true if the PoV hat is
+     * equal to the given angle, false
+     * otherwise
+     */
+    public boolean get() {
+    	return joystick.getPOV() == direction;
+    }
+    	
+    public enum Direction {
+    	right(90),
+    	upRight(45),
+    	up(0),
+    	upLeft(315),
+    	left(270),
+    	downLeft(225),
+    	down(180),
+    	downRight(135);
+    	
+    	private int dir;
+    	
+    	private Direction(int direction) {
+    	    dir = direction;
+    	}
+    	
+    	public int getDirection() {
+    	    return dir;
+    	}
+    }
+}
