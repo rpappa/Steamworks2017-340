23f1f74bab82374c426f7b1c12485862213f6951
wundrweapon <whhacker.dcx@gmail.com>
2017-01-16 11:21:21 -0500

    Set up drive code

---
+++ src/org/usfirst/frc/team340/robot/commands/DriveXbox.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team340.robot.commands;
+
+import org.usfirst.frc.team340.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Command for driving with Xbox 360 controller
+ */
+public class DriveXbox extends Command {
+
+    /**
+     * Constructs a new instance of
+     * {@link DriveXbox}, and all it does
+     * is set the required subsystem
+     */
+    public DriveXbox() {
+        requires(Robot.drive);
+    }
+
+    @Override
+    protected void initialize() {
+	Robot.drive.setBothDrive(0);
+    }
+
+    @Override
+    protected void execute() {
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+	Robot.drive.setBothDrive(0);
+    }
+
+    @Override
+    protected void interrupted() {
+	end();
+    }
+}
--- src/org/usfirst/frc/team340/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team340/robot/subsystems/Drive.java
@@ -1,13 +1,31 @@
 package org.usfirst.frc.team340.robot.subsystems;
 
 import org.usfirst.frc.team340.robot.RobotMap;
+import org.usfirst.frc.team340.robot.commands.DriveXbox;
 
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Drive {
+/**
+ * Subsystem that controls the drive train
+ */
+public class Drive extends Subsystem {
     private static Talon leftDrive = new Talon(RobotMap.LEFT_DRIVE_PORT);
     private static Talon rightDrive = new Talon(RobotMap.RIGHT_DRIVE_PORT);
     
+    /**
+     * Set the default command
+     */
+    @Override
+    public void initDefaultCommand() {
+        setDefaultCommand(new DriveXbox());
+    }
+    
+    /**
+     * Set the left drive speed
+     * @param speed percentage of full
+     * speed to go at [-1 ~ 1]
+     */
     public void setLeftDrive(double speed) {
 	if(speed < -1) {
 	    speed = -1;
@@ -18,6 +36,12 @@ public class Drive {
 	leftDrive.set(speed);
     }
     
+    /**
+     * Set the right drive speed; accounts
+     * for negation
+     * @param speed percentage of full
+     * speed to go at [-1 ~ 1]
+     */
     public void setRightDrive(double speed) {
 	if(speed < -1) {
 	    speed = -1;
@@ -28,10 +52,22 @@ public class Drive {
 	rightDrive.set(-speed);
     }
     
+    /**
+     * Set both drive rails to the same
+     * speed
+     * @param speed the speed to set both
+     * rails to
+     */
     public void setBothDrive(double speed) {
 	setBothDrive(speed, speed);
     }
     
+    /**
+     * Set each drive rail's speed
+     * separately
+     * @param lSpeed left speed
+     * @param rSpeed right speed
+     */
     public void setBothDrive(double lSpeed, double rSpeed) {
 	setLeftDrive(lSpeed);
 	setRightDrive(rSpeed);
