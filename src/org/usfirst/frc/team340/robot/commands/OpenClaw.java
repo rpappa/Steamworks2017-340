21edfaef491657d849167231073187ccaa576ab7
wundrweapon <whhacker.dcx@gmail.com>
2017-01-21 17:13:29 -0500

    Git indentation

--- src/org/usfirst/frc/team340/robot/commands/OpenClaw.java
+++ src/org/usfirst/frc/team340/robot/commands/OpenClaw.java
@@ -5,12 +5,12 @@ import org.usfirst.frc.team340.robot.Robot;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 
 public class OpenClaw extends InstantCommand {
-	public OpenClaw() {
-		requires(Robot.claw);
-	}
-	
-	@Override
-	protected void execute() {
-		Robot.claw.setClaw(false);
-	}
+    public OpenClaw() {
+	requires(Robot.claw);
+    }
+    
+    @Override
+    protected void execute() {
+	Robot.claw.setClaw(false);
+    }
 }
