372f04740bdfde49629e5919e9aa40bfeb4a9d25
CraftSpider <runetynan@gmail.com>
2017-01-21 15:57:08 -0500

    Created BranchingCommand abstract class extending Command

---
+++ src/edu/wpi/forst/wpilibj/command/BranchingCommand.java
@@ -0,0 +1,82 @@
+package edu.wpi.forst.wpilibj.command;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.ConditionalCommand;
+import edu.wpi.first.wpilibj.command.InstantCommand;
+
+/**
+ * <h1><em>BranchingCommand</em></h1><br>
+ * The meaning of this class is to create a
+ * {@link Command} that can be branched out into two
+ * possible outputs for the next {@link Command} to
+ * be run.<br>
+ * This type of {@link Command} functions exactly like
+ * any other, with some extra little bits added on.
+ * Specifically, it will automatically call the
+ * constructor-passed {@link ConditionalCommand} on
+ * its end.<br>
+ * <br>
+ * <em>Important things to note:</em>
+ * <ul>
+ * <li>{@link #end()} is NEVER to be
+ * overridden!
+ * <li>Rather than using {@link #end()}, you are required
+ * to use {@link #preEnd()}, as {@link #end()} will automatically
+ * invoke it before starting the {@link ConditionalCommand}.
+ * <li>{@link #interrupted()} will simply call {@link #end()},
+ * as that's what it defaults to in {@link Command#interrupted()}.
+ * Feel free to override it for any other purpose, of course.
+ * </ul>
+ */
+public abstract class BranchingCommand extends Command {
+	private ConditionalCommand next;
+	
+	/**
+	 * Create a new {@link BranchingCommand} and
+	 * define the {@link ConditionalCommand} to
+	 * be run afterwards
+	 * @param cmd the {@link ConditionalCommand}
+	 * to run after this
+	 */
+	public BranchingCommand(ConditionalCommand cmd) {
+		this.setNextCommand(cmd);
+	}
+	
+	/**
+	 * This will create a new BranchCommand that 
+	 * is simply a pass through and to support 
+	 * leaf nodes in our branching.
+	 */
+	public BranchingCommand(){
+		this.setNextCommand(new ConditionalCommand(new InstantCommand()) {
+			@Override
+			protected boolean condition() {
+				return true;
+			}
+		});
+	}
+	
+	private void setNextCommand(ConditionalCommand next){
+		this.next = next;
+	}
+	/**
+	 * The replacement for {@link Command#end()}
+	 * for derivative use. This is where you may want to wrap up
+	 * loose ends, like shutting off a motor that was being used
+	 * in the command.<br>
+	 * Yes, I <em>did</em> just steal that from {@link Command#end()}
+	 */
+	protected abstract void preEnd();
+	
+	/**
+	 * Called when the command ended peacefully. This is never
+	 * to be overridden, as doing so may break the "branching"
+	 * part of "branching command"
+	 * @see BranchingCommand#preEnd()
+	 */
+	@Override
+	protected void end() {
+		preEnd();
+		next.start();
+	}
+}
