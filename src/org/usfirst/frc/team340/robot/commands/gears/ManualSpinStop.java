f9b85910c2264a42cab887c9870ad5b690e1c15b
rpappa <rjp7@majorrom.com>
2017-01-30 19:15:22 -0500

    Added the manuals and the abort commands

---
+++ src/org/usfirst/frc/team340/robot/commands/gears/ManualSpinStop.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team340.robot.commands.gears;
+
+import org.usfirst.frc.team340.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ManualSpinStop extends Command {
+
+    public ManualSpinStop() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.claw);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.claw.spinStop();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return Robot.claw.isStopped();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
