fc0206c44bbb119503905f435dc2275c746762ee
wundrweapon <whhacker.dcx@gmail.com>
2017-02-02 19:04:37 -0500

    Changed climber speed controller to Spark

--- src/org/usfirst/frc/team340/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team340/robot/subsystems/Climber.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team340.robot.subsystems;
 
 import org.usfirst.frc.team340.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -12,10 +12,14 @@ public class Climber extends Subsystem {
 	
 	private static final double ENGAGEMENT_SPEED = 0.5;
 	private static final double CLIMB_SPEED = 0.75;
-	private Talon drum;
+	private Spark drumOne;
+	
+	@SuppressWarnings("unused")
+	private Spark drumTwo; //TODO: see if we need to worry about both (check for sync)
 	
 	public Climber() {
-		drum = new Talon(RobotMap.CLIMBER_DRUM_PORT);
+		drumOne = new Spark(RobotMap.CLIMBER_DRUM_PORT_ONE);
+		drumTwo = new Spark(RobotMap.CLIMBER_DRUM_PORT_TWO);
 	}
 
     // Put methods for controlling this subsystem
@@ -25,7 +29,7 @@ public class Climber extends Subsystem {
 	}
 	
 	private void goToSpeed(double speed){
-		drum.set(speed);
+		drumOne.set(speed);
 	}
 	
 	public boolean isEngagedWithRope(){
@@ -45,7 +49,7 @@ public class Climber extends Subsystem {
 	}
 	
 	public boolean isStopped(){
-		return drum.get() ==0;
+		return drumOne.get() ==0;
 	}
 
     public void initDefaultCommand() {
@@ -53,4 +57,3 @@ public class Climber extends Subsystem {
         //setDefaultCommand(new MySpecialCommand());
     }
 }
-
