a382bd73e7b90f1cab4cbd91c4bc8fb7adb86eb7
wundrweapon <whhacker.dcx@gmail.com>
2017-02-02 19:05:00 -0500

    Shelled out Claw subsystem

--- src/org/usfirst/frc/team340/robot/RobotMap.java
+++ src/org/usfirst/frc/team340/robot/RobotMap.java
@@ -13,9 +13,14 @@ public class RobotMap {
     public static final int RIGHT_DRIVE_PORT = 1;
     public static final int CLAW_ROLLERS_PORT = 2;
     public static final int DROP_SOLENOID_CHANNEL = 3;
-    public static final int ARM_SOLENOID_CHANNEL = 4;
-    public static final int CLAW_SOLENOID_CHANNEL = 5;
-    public static final int CLIMBER_DRUM_PORT = 6;
+    public static final int ARM_SOLENOID_FORWARD_CHANNEL = 4;
+    public static final int ARM_SOLENOID_REVERSE_CHANNEL = 5;
+    public static final int CLAW_SOLENOID_FORWARD_CHANNEL = 7;
+    public static final int CLAW_SOLENOID_REVERSE_CHANNEL = 8;
+    public static final int PUSHER_SOLENOID_FORWARD_CHANNEL = 9;
+    public static final int PUSHER_SOLENOID_REVERSE_CHANNEL = 10;
+    public static final int CLIMBER_DRUM_PORT_ONE = 11;
+    public static final int CLIMBER_DRUM_PORT_TWO = 12;
     
 	public static final double CLIMB_MOTORS_ENGAGEMENT_SPIN_UP_TIME = 0.5;
 }
--- src/org/usfirst/frc/team340/robot/subsystems/Claw.java
+++ src/org/usfirst/frc/team340/robot/subsystems/Claw.java
@@ -1,89 +1,100 @@
 package org.usfirst.frc.team340.robot.subsystems;
 
+import org.usfirst.frc.team340.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- *
- */
 public class Claw extends Subsystem {
-
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	private static final double ROLLER_SPEED = .75; //TODO: perfect this
+	
+	private DoubleSolenoid claw;
+	private DoubleSolenoid hinge;
+	private DoubleSolenoid pusher;
+	private TalonSRX rollers;
+	
+	public Claw() {
+		claw = new DoubleSolenoid(RobotMap.CLAW_SOLENOID_FORWARD_CHANNEL, RobotMap.CLAW_SOLENOID_REVERSE_CHANNEL);
+		hinge = new DoubleSolenoid(RobotMap.ARM_SOLENOID_FORWARD_CHANNEL, RobotMap.ARM_SOLENOID_REVERSE_CHANNEL);
+		pusher = new DoubleSolenoid(RobotMap.PUSHER_SOLENOID_FORWARD_CHANNEL, RobotMap.PUSHER_SOLENOID_REVERSE_CHANNEL);
+		rollers = new TalonSRX(RobotMap.CLAW_ROLLERS_PORT);
+	}
+    
 	public void goUp(){
-		
+		hinge.set(Value.kReverse);
 	}
 
 	public boolean isUp(){
-		return false;
+		return hinge.get().equals(Value.kReverse);
 	}
 	
 	public void goDown(){
-		
+		hinge.set(Value.kForward);
 	}
 	
 	public boolean isDown(){
-		return false;
+		return hinge.get().equals(Value.kForward);
 	}
 	
 	public void goOpen(){
-		
+		claw.set(Value.kForward); //TODO: see if forward is open or closed
 	}
 	
 	public boolean isOpened(){
-		return false;
+		return claw.get().equals(Value.kForward);
 	}
 	
 	public void goClose(){
-		
+		claw.set(Value.kReverse);
 	}
 	
 	public boolean isClosed(){
-		return false;
+		return claw.get().equals(Value.kReverse);
 	}
 	
 	public void goRetract(){
-		
+		pusher.set(Value.kReverse);
 	}
 	
 	public boolean isRetracted(){
-		return false;
+		return pusher.get().equals(Value.kReverse);
 	}
 	
 	public void goExtend(){
-		
+		pusher.set(Value.kForward);
 	}
 	
 	public boolean isExtended(){
-		return false;
+		return pusher.get().equals(Value.kForward);
 	}
 	
 	public void spinStop(){
-		
+		rollers.set(0); //0 because no speed is stopped
 	}
 	
 	public boolean isStopped(){
-		return false;
+		return rollers.getSpeed() == 0;
 	}
 	
 	public void spinIn(){
-		
+		rollers.set(ROLLER_SPEED);
 	}
 	
 	public boolean whenGearIsAcquired(){
-		return false;
+		return false; //TODO: make this
 	}
 	
 	public void spinOut(){
-		
+		rollers.set(-ROLLER_SPEED); //Negative to push out
 	}
 	
 	public boolean whenGearIsNotAcquired(){
-		return false;
+		return false; //TODO: make this
 	}
 	
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+        //No default command needed
     }
 }
-
