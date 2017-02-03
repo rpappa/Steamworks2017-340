e27c71143e39313d958d6915261aef768db43de7
wundrweapon <whhacker.dcx@gmail.com>
2017-02-02 19:32:37 -0500

    Improve the claw shell

--- src/org/usfirst/frc/team340/robot/subsystems/Claw.java
+++ src/org/usfirst/frc/team340/robot/subsystems/Claw.java
@@ -7,14 +7,34 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+/**
+ * <h1><em>Claw</em></h1>
+ * <br>
+ * This class handles the mechanisms of the claw:
+ * the pusher piston, arm, and claw itself.
+ */
 public class Claw extends Subsystem {
-	private static final double ROLLER_SPEED = .75; //TODO: perfect this
+	private static final double ROLLER_IN_SPEED = .75; //TODO: perfect this
+	private static final double ROLLER_OUT_SPEED = -.75; //TODO: perfect this too
+	
+	private static final Value ARM_UP = Value.kReverse;
+	private static final Value ARM_DOWN = Value.kForward;
+	private static final Value CLAW_OPEN = Value.kForward; //TODO: see if forward is open or closed
+	private static final Value CLAW_CLOSED = Value.kReverse; //TODO: see if reverse is open or closed
+	private static final Value PUSHER_OUT = Value.kForward;
+	private static final Value PUSHER_IN = Value.kReverse;
 	
 	private DoubleSolenoid claw;
 	private DoubleSolenoid hinge;
 	private DoubleSolenoid pusher;
 	private TalonSRX rollers;
 	
+	/**
+	 * Constructs a {@link Claw} and sets all
+	 * the instance variables = they being a
+	 * speed controller and three double
+	 * solenoids
+	 */
 	public Claw() {
 		claw = new DoubleSolenoid(RobotMap.CLAW_SOLENOID_FORWARD_CHANNEL, RobotMap.CLAW_SOLENOID_REVERSE_CHANNEL);
 		hinge = new DoubleSolenoid(RobotMap.ARM_SOLENOID_FORWARD_CHANNEL, RobotMap.ARM_SOLENOID_REVERSE_CHANNEL);
@@ -22,78 +42,143 @@ public class Claw extends Subsystem {
 		rollers = new TalonSRX(RobotMap.CLAW_ROLLERS_PORT);
 	}
     
-	public void goUp(){
-		hinge.set(Value.kReverse);
+	/**
+	 * Raise the "arm" (the entire claw)
+	 */
+	public void goUp() {
+		hinge.set(ARM_UP);
 	}
 
-	public boolean isUp(){
-		return hinge.get().equals(Value.kReverse);
+	/**
+	 * @return if the "arm" (the entire claw) is
+	 * pulled up
+	 */
+	public boolean isUp() {
+		return hinge.get().equals(ARM_UP);
 	}
 	
-	public void goDown(){
-		hinge.set(Value.kForward);
+	/**
+	 * Drop the "arm" (the entire claw)
+	 */
+	public void goDown() {
+		hinge.set(ARM_DOWN);
 	}
 	
-	public boolean isDown(){
-		return hinge.get().equals(Value.kForward);
+	/**
+	 * @return if the "arm" (the entire claw) is
+	 * pushed down
+	 */
+	public boolean isDown() {
+		return hinge.get().equals(ARM_DOWN);
 	}
 	
-	public void goOpen(){
-		claw.set(Value.kForward); //TODO: see if forward is open or closed
+	/**
+	 * Open the claw
+	 */
+	public void goOpen() {
+		claw.set(CLAW_OPEN);
 	}
 	
-	public boolean isOpened(){
-		return claw.get().equals(Value.kForward);
+	/**
+	 * @return if the claw if open
+	 */
+	public boolean isOpened() {
+		return claw.get().equals(CLAW_OPEN);
 	}
 	
-	public void goClose(){
-		claw.set(Value.kReverse);
+	/**
+	 * Close the claw
+	 */
+	public void goClose() {
+		claw.set(CLAW_CLOSED);
 	}
 	
-	public boolean isClosed(){
-		return claw.get().equals(Value.kReverse);
+	/**
+	 * @return if the claw is closed
+	 */
+	public boolean isClosed() {
+		return claw.get().equals(CLAW_CLOSED);
 	}
 	
-	public void goRetract(){
-		pusher.set(Value.kReverse);
+	/**
+	 * Retract the pusher
+	 */
+	public void goRetract() {
+		pusher.set(PUSHER_IN);
 	}
 	
-	public boolean isRetracted(){
-		return pusher.get().equals(Value.kReverse);
+	/**
+	 * @return if the pusher is
+	 * retracted
+	 */
+	public boolean isRetracted() {
+		return pusher.get().equals(PUSHER_IN);
 	}
 	
-	public void goExtend(){
-		pusher.set(Value.kForward);
+	/**
+	 * Extend the pusher
+	 */
+	public void goExtend() {
+		pusher.set(PUSHER_OUT);
 	}
 	
-	public boolean isExtended(){
-		return pusher.get().equals(Value.kForward);
+	/**
+	 * @return if the pusher is
+	 * extended
+	 */
+	public boolean isExtended() {
+		return pusher.get().equals(PUSHER_OUT);
 	}
 	
-	public void spinStop(){
+	/**
+	 * Stop the rollers
+	 */
+	public void spinStop() {
 		rollers.set(0); //0 because no speed is stopped
 	}
 	
-	public boolean isStopped(){
+	/**
+	 * @return if the rollers are stopped
+	 */
+	public boolean isStopped() {
 		return rollers.getSpeed() == 0;
 	}
 	
-	public void spinIn(){
-		rollers.set(ROLLER_SPEED);
+	/**
+	 * Spin the rollers to pull in a gear
+	 */
+	public void spinIn() {
+		rollers.set(ROLLER_IN_SPEED);
 	}
 	
-	public boolean whenGearIsAcquired(){
+	/**
+	 * @return if the gear has
+	 * successfully been acquired
+	 */
+	public boolean whenGearIsAcquired() {
 		return false; //TODO: make this
 	}
 	
-	public void spinOut(){
-		rollers.set(-ROLLER_SPEED); //Negative to push out
+	/**
+	 * Spin the rollers to
+	 * regurgitate the gear
+	 */
+	public void spinOut() {
+		rollers.set(ROLLER_OUT_SPEED); //Negative to push out
 	}
 	
-	public boolean whenGearIsNotAcquired(){
-		return false; //TODO: make this
+	/**
+	 * @return if the gear has NOT
+	 * successfully been acquired
+	 */
+	public boolean whenGearIsNotAcquired() {
+		return !whenGearIsAcquired();
 	}
 	
+	/**
+	 * Sets the default command by
+	 * not setting a default command
+	 */
     public void initDefaultCommand() {
         //No default command needed
     }
